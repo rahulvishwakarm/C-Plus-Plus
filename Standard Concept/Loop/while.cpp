0acef5a1a446ffeaf013b266427ffbe4a9225d5b
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-06 21:02:04 +0530

    Control Flow

---
+++ Standard Concept/Loop/while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num = 6;
+    while (num<12)
+    {
+        cout<<num<<endl;
+        num++;
+    }
+    getch();
+}
