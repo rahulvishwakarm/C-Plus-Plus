ae9044602dfcb65d26ec21745aed51058f8822f5
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-06 19:01:42 +0530

    Variable

---
+++ Standard Concept/Variables/variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=32;
+    const char *tst = "test";  // This is the way to assign long string in variable
+    float f = 6347.89;
+    cout<<"This is Integer:"<<a<<endl;
+    cout<<"This is Character:"<<tst<<endl;
+    cout<<"This is Float:"<<f;
+}
