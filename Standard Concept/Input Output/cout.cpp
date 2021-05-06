d696343ac8df792b65bb5af3fa6b6317b27388f6
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-06 18:47:51 +0530

    Input Output

---
+++ Standard Concept/Input Output/cout.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char arr[] = "Welcome to C plus plus";
+    cout<<"Testing my application\n";
+    cout<<"Value in array are:"<<arr<<endl;
+    cout<<"After array line";
+}
---
+++ Standard Concept/Input Output/endl.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"This is first line"<<endl;
+    cout<<"This is second line"<<endl;
+    cout<<"This is third line";
+}
