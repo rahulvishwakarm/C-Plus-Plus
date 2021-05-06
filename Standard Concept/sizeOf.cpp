3359e688296517b43aa7bdbfd2fd9b723ffe027f
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-07 00:51:39 +0530

    Size Of Data Types

---
+++ Standard Concept/sizeOf.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Size of Data Types" <<endl;
+    cout << "Size of int: "<<sizeof(int)<<endl;
+    cout << "Size of float: "<<sizeof(float)<<endl;
+    cout << "Size of double: "<<sizeof(double)<<endl;
+    cout << "Size of char: "<<sizeof(char)<<endl;
+}
