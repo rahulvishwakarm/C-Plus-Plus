654e3242cbdcb911194e48a9c1cb5ecc3a612f46
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-08 02:16:01 +0530

    String Operation - 1

---
+++ Standard Concept/String/string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    string s1 = "Hello";
+    char ch[] = {'C','+','+'};
+    string s2 = (string)ch;
+    cout<<s1<<endl;
+    cout<<s2<<endl;
+}
