7c953425d904148bb3fd0f075515bfa5749177d3
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-27 21:32:56 +0530

    Doubly Linked List - Deletion From End

---
+++ Data Structures/Linked List/Doubly Linked List/DeletionFromEnd.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Node
+{   
+    public:
+        int data;
+        Node *prev;
+        Node *next;
+};
+
+Node *head = NULL;
+Node *last = NULL;
+Node *current = NULL;
+
+void insert(int d)
+{
+    Node *link = new Node();
+    link->data = d;
+    link->prev = NULL;
+    link->next = NULL;
+
+    if(head==NULL)
+    {
+        head = link;
+        return;
+    }
+    current = head;
+
+    while (current->next!=NULL)
+    {
+        current = current->next;
+    }
+    current->next = link;
+    last = link;
+    link->prev = current;
+};
+
+void deleteFromEnd()
+{   
+    Node *deleteptr = head;
+    while (deleteptr->next!=NULL)
+    {
+        deleteptr = deleteptr->next;
+        if(deleteptr->next->next==NULL)
+        {
+            deleteptr->next = NULL;
+        }
+    }
+    
+};
+
+void printList()
+{
+    Node *ptr = head;
+    cout<<"[head]";
+    while (ptr->next!=NULL)
+    {
+        cout<<"<=>"<<ptr->data;
+        ptr = ptr->next;    
+    }
+    cout<<"<=>"<<ptr->data;
+    cout<<"<=>[head]\n";
+};
+
+int main()
+{
+    insert(11);
+    insert(22);
+    insert(33);
+    insert(44);
+    insert(55);
+    printList();
+    deleteFromEnd();
+    printList();
+}
