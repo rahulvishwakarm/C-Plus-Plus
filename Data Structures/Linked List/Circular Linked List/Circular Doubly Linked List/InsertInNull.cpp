34224ae37f585d8d31af956fc9491233c72699bc
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-06-02 00:40:04 +0530

    Circular Doubly Linked List - Insertion In Empty

---
+++ Data Structures/Linked List/Circular Linked List/Circular Doubly Linked List/InsertInNull.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node *prev;
+        Node *next;
+};
+
+Node *addToNull(Node *head,int data)
+{
+    if(head!=NULL)
+    {
+        return head;
+    }
+    Node *lnk = new Node();
+    lnk->data = data;
+    lnk->next = NULL;
+    lnk->prev = NULL;
+    head = lnk;
+    lnk->next = head;
+    return head;
+};
+
+void traverse(Node *head)
+{
+    Node *ptr = head->next;
+    cout<<"[head]<=>";
+    do
+    {
+        cout<<ptr->data<<"<=>";
+        ptr = ptr->next;
+    } while (ptr!=head->next);
+    cout<<"[head]";
+}
+
+int main()
+{
+    Node *head = NULL;
+    head = addToNull(head,11);
+    traverse(head);
+}
